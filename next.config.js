c8ff5bbc641312a0ff3504b31ee5a9566b7d732c
josephchalifoux <joseph.chalifoux@yahoo.com>
2025-10-13 10:11:17 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,2 +1,6 @@
 /** @type {import('next').NextConfig} */
-module.exports = { reactStrictMode: true };
+const nextConfig = {
+reactStrictMode: true,
+experimental: { typedRoutes: true }
+};
+module.exports = nextConfig;
