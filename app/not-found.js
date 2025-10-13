8a36c299cec87cb75c5e22781ce64d92c340205e
josephchalifoux <joseph.chalifoux@yahoo.com>
2025-10-12 21:36:27 -0400

    Create not-found.js

---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <main style={{padding:24, fontFamily:'system-ui, sans-serif'}}>
+      <h1>404</h1>
+      <p>We couldn’t find that page.</p>
+      <p><a href="/">Go home</a></p>
+    </main>
+  );
+}
