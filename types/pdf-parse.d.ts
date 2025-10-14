83a47781f349dacd915f439c2fb3fe511202758b
josephchalifoux <joseph.chalifoux@yahoo.com>
2025-10-14 11:12:14 -0400

    Update pdf-parse.d.ts

--- types/pdf-parse.d.ts
+++ types/pdf-parse.d.ts
@@ -1,17 +1,16 @@
+// types/pdf-parse.d.ts
 declare module "pdf-parse" {
   export interface PDFParseResult {
     text: string;
     numpages?: number;
     numrender?: number;
-    info?: any;
-    metadata?: any;
+    info?: unknown;
+    metadata?: unknown;
     version?: string;
   }
 
   type Data = Buffer | Uint8Array | ArrayBuffer;
 
   function pdfParse(data: Data, options?: any): Promise<PDFParseResult>;
-
-  // CommonJS default export
   export default pdfParse;
 }
