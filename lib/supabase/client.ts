b84c15885ff55c25129712faffc6664662266d2c
josephchalifoux <joseph.chalifoux@yahoo.com>
2025-10-14 12:54:35 -0400

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,9 +1,6 @@
-'use client'
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-export const supabase = createClient(url, anon, {
-  auth: { persistSession: true, autoRefreshToken: true },
-})
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
