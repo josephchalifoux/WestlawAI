411f54600dc2a6fae536ef7a51ba41cb9e7c1191
josephchalifoux <joseph.chalifoux@yahoo.com>
2025-10-13 22:54:52 -0400

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+// app/robots.ts
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  const host = "https://westlawai.com";
+  return {
+    rules: [{ userAgent: "*", allow: "/" }],
+    sitemap: `${host}/sitemap.xml`,
+    host,
+  };
+}
